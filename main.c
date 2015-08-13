1054e7e3913e0e40db755b45f313eb764ccb7652
test <a@b.com>
2015-08-13 12:39:57 -0500

    Se modificó main.c, se llamaron las funciones

--- main.c
+++ main.c
@@ -1,8 +1,47 @@
 #include <stdio.h>
 #include "calculadora.h"
 
+int a,b,op;
+
 int main (void)
 {
+
+printf("Ingrese los números que desea operar (con comas):\n ");
+scanf("%d,%d", &a,&b);
+
+printf("Qué operación desea realizar \n1-Suma\n2-Resta\n3-Multiplicación\n4-División");
+scanf("%d",&op);
+
+switch(op)
+
+{
+
+case 1: 
+
+sum(a,b);
+
+break;
+
+case 2: 
+
+res(a,b);
+
+break;
+
+case 3: 
+
+mult(a,b);
+
+break;
+
+
+case 4: 
+
+div(a,b);
+
+break;
+
+}
 
 return 0;
 
