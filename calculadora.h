b1e532f804e28cb4dfb85e919214d51da5ae006c
test <a@b.com>
2015-08-13 12:17:26 -0500

    Se edito calculadora.h colocándo las funciones

---
+++ calculadora.h
@@ -0,0 +1,41 @@
+main.c
+
+#include " calculadora.h"
+
+
+int sum (int a, int b);
+
+{
+
+return a+b ;
+
+}
+
+
+int res (int a, int b);
+
+{
+
+return a-b ;
+
+}
+
+
+int mult(int a, int b);
+
+{
+
+
+return a*b ;
+
+}
+
+
+int div(int a, int b);
+
+{
+
+
+return a/b ;
+
+}
